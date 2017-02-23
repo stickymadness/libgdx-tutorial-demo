cbb5c1c94059038b9cd1d40735807853923ea638
Tomaz Ravljen <tomaz.ravljen@equaleyes.com>
2017-02-23 22:09:35 +0100

    cleared imports

--- core/src/com/game/demo/screens/MenuScreen.java
+++ core/src/com/game/demo/screens/MenuScreen.java
@@ -5,15 +5,10 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Event;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.game.demo.GameMain;
 
 public class MenuScreen extends AbstractScreen {
